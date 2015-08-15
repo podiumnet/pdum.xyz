e04e655fb2abe1f8c5e982a05d2735e8cdf505cd
htmlguy <htmlguy999@gmail.com>
2015-08-15 19:05:13 -0400

    Recompile

--- compiled/server/server.js
+++ compiled/server/server.js
@@ -5,6 +5,7 @@
     app = express();
     environment = require('./environment');
     app.use(express["static"](maindir + "/web"));
+    app.use(express["static"](maindir + "/compiled/browser"));
     return server = app.listen(environment.port, environment.ip, function() {
       return console.log('PODIUM SERVER Listening @ %s:%s', environment.ip, environment.port);
     });
