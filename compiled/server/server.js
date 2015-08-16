578157d1cda9a597ee59c36d23a3a7332b783089
htmlguy <htmlguy999@gmail.com>
2015-08-16 08:50:13 -0400

    Access Control

--- compiled/server/server.js
+++ compiled/server/server.js
@@ -4,6 +4,13 @@
     express = require('express');
     app = express();
     environment = require('./environment');
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header('Access-Control-Allow-Credentials', true);
+      res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+      return next();
+    });
     app.use(express["static"](maindir + "/web"));
     app.use(express["static"](maindir + "/compiled/browser"));
     return server = app.listen(environment.port, environment.ip, function() {
--- src/server/server.coffee
+++ src/server/server.coffee
@@ -3,6 +3,13 @@ module.exports = (maindir)->
   app = express()
   environment = require './environment'
 
+  app.use (req, res, next) ->
+    res.header "Access-Control-Allow-Origin", "*"
+    res.header 'Access-Control-Allow-Credentials', true
+    res.header 'Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS'
+    res.header 'Access-Control-Allow-Headers', 'Content-Type'
+    next()
+
   app.use express.static "#{maindir}/web"
   app.use express.static "#{maindir}/compiled/browser"
 
